999bb81d399ecd9a2bdeea1a8b8cc34c6e1b10d6
Fernando Daciuk <f.daciuk@gmail.com>
2023-06-28 11:12:51 -0300

    feat: add middle (left and right) camera on cam-screen page

--- src/app/single-cam-screen/page.tsx
+++ src/app/single-cam-screen/page.tsx
@@ -3,7 +3,7 @@ import { Cam, Layout } from '@/ui'
 type PageProps = {
   searchParams: {
     cam: string
-    cam_position: 'top_left' | 'top_right' | 'bottom_right' | 'bottom_left'
+    cam_position: 'top_left' | 'top_right' | 'bottom_right' | 'bottom_left' | 'middle_left' | 'middle_right'
   }
 }
 
@@ -15,6 +15,8 @@ export default function SingleCamWithScreen({ searchParams }: PageProps) {
           absolute
           ${searchParams.cam_position === 'top_left' ? 'top-6 left-6' : ''}
           ${searchParams.cam_position === 'top_right' ? 'top-6 right-6' : ''}
+          ${searchParams.cam_position === 'middle_left' ? 'top-[50%] -mt-[calc(196px/2)] left-6' : ''}
+          ${searchParams.cam_position === 'middle_right' ? 'top-[50%] -mt-[calc(196px/2)] right-6' : ''}
           ${searchParams.cam_position === 'bottom_right' ? 'bottom-6 right-6' : ''}
           ${searchParams.cam_position === 'bottom_left' ? 'bottom-6 left-6' : ''}
         `}
